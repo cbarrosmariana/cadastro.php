e31b16e5d7fc7cfbc356f4eb29ab1252057f0290
marideveloper <mbarroscribeiro@hotmail.com>
2016-05-19 18:03:56 -0300

    cadastro
    formulario de cadastro php

---
+++ cadastrando.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<title>Cdastrando...</title>
+	</head>
+	<body>
+		<?php
+			$host = "localhost";
+			$user = "root";
+			$pass = "";
+			$banco = "cadastro";
+			$conexao = mysql_connect($host, $user, $pass) or die(mysql_error());
+			mysql_select_db($banco) or die(mysql_error());
+			?>
+
+			<?php
+			$nome=$_POST['nome'];
+			$sobrenome=$_POST['sobrenome'];
+			$pais=$_POST['pais'];
+			$estado=$_POST['estado'];
+			$cidade=$_POST['cidade'];
+			$email=$_POST['email'];
+			$senha=$_POST['senha'];
+			$sql = mysql_query("INSERT INTO usuarios(nome, sobrenome, pais, estado, cidade, email, senha)
+			VALUES('$nome', '$sobrenome', '$pais', '$estado', '$cidade', '$email', '$senha')");
+			echo "<center><h1>Cadastro efetuado com sucesso!</h1></center";
+		?>
+	</body>
+</html>
+
+
+
---
+++ cadastro.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Sistemas de Cadastro</title>
+</head>
+<body>
+	<form name="signup" method="post" action="cadastrando.php">
+		Nome: <input type="text" name="nome" /></br></br>
+		Sobrenome: <input type="text" name="sobrenome" /></br></br>
+		Pais: <input type="text" name="pais"/></br></br>
+		Estado: <input type="text" name="estado"/></br></br>
+		Cidade: <input type="text" name="cidade" /></br></br>
+		E-mail: <input type="text" name="email"/></br></br>
+		Senha: <input type="password" name="senha" /></br></br>
+		<input type="submit" value="cadastrar"/>
+	</form>
+</body>
+</html>
